ce1d560cdb9d5acd21e5066cd8cf5a069b8a0e55
Nicolas Potel <nicolas@potel.dev>
2026-08-28 18:24:52 +0200

    remove old migrations

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,13 +3,8 @@ import { NestFactory } from '@nestjs/core';
 import { json } from 'express';
 import { AppModule } from './app.module';
 import { backfillThumbnails } from './item/backfill-thumbnails';
-import { migrateImageToImagesArray } from './item/migrate-images-array';
-import { migrateReleaseDateToReleaseYear } from './item/migrate-release-year';
 
 async function bootstrap() {
-  const databasePath = process.env.DATABASE_PATH ?? 'inventory.sqlite';
-  await migrateReleaseDateToReleaseYear(databasePath);
-  await migrateImageToImagesArray(databasePath);
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors();
